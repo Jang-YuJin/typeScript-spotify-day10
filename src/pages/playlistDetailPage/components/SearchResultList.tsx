009fe6bef223405f31959661b89b3082831e6822
Jang-YuJin <jangyj0912@naver.com>
2026-01-03 22:34:37 +0900

    타입스크립트 스포티파이 프로젝트 10일차

---
+++ src/pages/playlistDetailPage/components/SearchResultList.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './SearchResultList.style.css'
+import { Track } from '../../../models/track';
+import { Button, Table, TableBody, TableCell, TableRow, Typography, useMediaQuery } from '@mui/material';
+import Loading from '../../../common/components/loading/Loading';
+
+interface SearchResultListProps {
+    list: Track[];
+    ref: (node?: Element | null | undefined) => void;
+    isFetchingNextPage: boolean;
+}
+
+const SearchResultList = ({list, ref, isFetchingNextPage}: SearchResultListProps) => {
+  const isMobile = useMediaQuery("(max-width:767px)");
+
+  return (
+    <TableBody className='searchResultTable'>
+      {list.map((track, index) => (
+        <TableRow key={index}>
+          <TableCell>
+            <span className='searchResultImgContainer'>
+              <img className='searchResultImg' src={track.album?.images[0].url ?? '/noimg.png'}></img>
+              <span className='searchResultImgInfoContainer'>
+                <Typography fontWeight={700}>{track.name ?? 'No name'}</Typography>
+                <Typography color='text.secondary'>{track.album?.artists[0].name ?? 'Unknown'}</Typography>
+              </span>
+            </span>
+            </TableCell>
+            {isMobile ? '' : <TableCell>{track.album?.name}</TableCell>}
+            <TableCell>
+              <Button variant='outlined'>추가</Button>
+            </TableCell>
+        </TableRow>
+      ))}
+      <div ref={ref}>{isFetchingNextPage && <Loading></Loading>}</div>
+    </TableBody>
+  )
+}
+
+export default SearchResultList
